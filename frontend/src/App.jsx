66611c13d0360ce1fac647f404e15c772ef19f56
ldc911 <lduclos@protonmail.com>
2023-02-02 03:18:14 +0100

    createsession quasi ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
+import CreateSession from "./pages/CreateSession";
 
 import "./index.css";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Home />
-      <p className="text-xs text-red-900 md:text-3xl">coucou</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<NavBar />} />
+        <Route path="" element={<Home />} />
+        <Route path="create" element={<CreateSession />} />
+      </Routes>
+    </Router>
   );
 }
 
