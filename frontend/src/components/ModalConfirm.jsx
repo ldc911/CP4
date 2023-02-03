156a5aa98e29c226a5c9663959c115d30618cfb9
Laurent Duclos <lduclos@protonmail.com>
2023-02-03 09:04:36 +0100

    fixed color issues

--- frontend/src/components/ModalConfirm.jsx
+++ frontend/src/components/ModalConfirm.jsx
@@ -61,7 +61,7 @@ export default function ModalConfirm({ data, showDeleteModal, handleClose }) {
                 <div className="sm:flex sm:items-start">
                   <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                     <ExclamationTriangleIcon
-                      className="h-6 w-6 text-red-600"
+                      className="h-6 w-6 text-red-900"
                       aria-hidden="true"
                     />
                   </div>
