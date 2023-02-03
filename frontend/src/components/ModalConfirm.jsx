ce1bad03ac7631a7ac82fb89ab8a527669c99afd
Laurent Duclos <lduclos@protonmail.com>
2023-02-03 10:02:27 +0100

    added blur on modals

--- frontend/src/components/ModalConfirm.jsx
+++ frontend/src/components/ModalConfirm.jsx
@@ -38,7 +38,7 @@ export default function ModalConfirm({ data, showDeleteModal, handleClose }) {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-60 backdrop-blur transition-opacity" />
           </Transition.Child>
 
           <span
@@ -85,7 +85,7 @@ export default function ModalConfirm({ data, showDeleteModal, handleClose }) {
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-800 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
                   onClick={handleDelete}
                 >
                   Supprimer
